064db422dc749b990ad47897fb463aebab8a530b
Anil Kumar Marella <amare012@al003110020.local>
2018-10-09 21:56:29 -0500

    Changed with switch

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
-for x in range(0, 3):
-    GPIO.output(7, True)
-    time.sleep(1)
-    GPIO.output(7, False)
-    GPIO.output(11, True)
-    time.sleep(1)
-    GPIO.output(11, False)
-    time.sleep(1)
+GPIO.setup()
+while True:
+	GPIO.output(7, True)
+    if GPIO.input(15):
+    	GPIO.output(7, False)
+    	GPIO.output(11, True)
+    else: 
+    	GPIO.output(11, False)
 
 GPIO.cleanup()
